9b45aac6d4a11e8b2b93a281361ca32057248630
Alex Brooks <albrooks@redhat.com>
2026-02-23 18:17:30 +0000

    log invalid dtypes
    Signed-off-by: Alex Brooks <albrooks@redhat.com>

--- vllm_omni/entrypoints/omni.py
+++ vllm_omni/entrypoints/omni.py
@@ -219,7 +219,7 @@ class OmniBase:
         # TODO: hack, convert dtype to string to avoid non-premitive omegaconf create error.
         if "dtype" in kwargs and not isinstance(kwargs["dtype"], str):
             if not isinstance(kwargs["dtype"], torch.dtype):
-                raise TypeError("Provided dtype must be a string or torch.dtype")
+                raise TypeError(f"Provided dtype must be a string or torch.dtype, got {type(kwargs['dtype']).__name__}")
             kwargs["dtype"] = str(kwargs["dtype"]).removeprefix("torch.")
 
         cache_backend = kwargs.get("cache_backend", "none")
