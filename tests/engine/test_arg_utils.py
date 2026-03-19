1e0614c9a493c1c4978fa84bfdfbf742eb9b0c18
Alex Brooks <albrooks@redhat.com>
2026-03-19 17:33:54 +0000

    remove omni async engine arg tests
    Signed-off-by: Alex Brooks <albrooks@redhat.com>

--- tests/engine/test_arg_utils.py
+++ tests/engine/test_arg_utils.py
@@ -5,68 +5,23 @@ explicitly patch values that differ from vLLM.
 """
 
 import inspect
-from dataclasses import asdict
 from unittest.mock import Mock
 
 import pytest
 from pydantic import ValidationError
 from transformers import PretrainedConfig
-from vllm.engine.arg_utils import AsyncEngineArgs, EngineArgs
+from vllm.engine.arg_utils import EngineArgs
 
 from vllm_omni.config.model import OmniModelConfig
-from vllm_omni.engine.arg_utils import AsyncOmniEngineArgs, OmniEngineArgs
+from vllm_omni.engine.arg_utils import OmniEngineArgs
 
 pytestmark = [pytest.mark.core_model, pytest.mark.cpu]
-
-
-def test_default_async_engine_args_match_sync_engine_args():
-    """Ensure that all keys/values in sync omni engine args are aligned
-    with async omni engine args, and that any keys in the async omni engine
-    args not in the sync omni args are aligned with vLLM's AsyncEngineArgs.
-    """
-    sync_vllm_dict = asdict(EngineArgs())
-    async_vllm_dict = asdict(AsyncEngineArgs())
-    diff_keys = async_vllm_dict.keys() - sync_vllm_dict.keys()
-    # Generally, EngineArgs keys are a subset of AsyncEngineArgs keys
-    assert len(sync_vllm_dict.keys() - async_vllm_dict.keys()) == 0
-
-    sync_omni_dict = asdict(OmniEngineArgs())
-    async_omni_dict = asdict(AsyncOmniEngineArgs())
-    for async_key in diff_keys:
-        # Any values in not in the omni sync dataclass should
-        # directly match the async vLLM async dataclass; remove
-        # it & compare.
-        assert async_key in async_vllm_dict
-        vllm_async_val = async_vllm_dict.pop(async_key)
-        omni_async_val = async_omni_dict.pop(async_key)
-        assert omni_async_val == vllm_async_val
-
-    # After removing async only keys from our AsyncOmniEngineArgs,
-    # we should have the same dict as the OmniEngineArgs.
-    assert async_omni_dict == sync_omni_dict
 
 
 def test_sync_config_is_omni():
     """Ensure create_model_config gives the right type."""
     cfg = OmniEngineArgs().create_model_config()
     assert isinstance(cfg, OmniModelConfig)
-
-
-def test_async_config_is_omni():
-    """Ensure create_model_config gives the right type (async args)."""
-    cfg = AsyncOmniEngineArgs().create_model_config()
-    assert isinstance(cfg, OmniModelConfig)
-
-
-def test_async_engine_args_mro():
-    """Ensure .mro is correct to prevent issues with config creation."""
-    mro = AsyncOmniEngineArgs.mro()
-    omni_eng_idx = mro.index(OmniEngineArgs)
-    async_args_idx = mro.index(AsyncEngineArgs)
-    # > 0 since the first entry is AsyncOmniEngineArgs
-    assert omni_eng_idx > 0
-    assert async_args_idx > 0
-    assert omni_eng_idx < async_args_idx
 
 
 def test_multimodal_kwarg_overrides():
