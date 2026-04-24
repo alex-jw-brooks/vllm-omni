3a36ad57b66eed554a0b01c754f9b2a0f7b1e6ea
Alex Brooks <albrooks@redhat.com>
2026-04-24 15:06:10 +0000

    fmt
    Signed-off-by: Alex Brooks <albrooks@redhat.com>

--- vllm_omni/entrypoints/omni_base.py
+++ vllm_omni/entrypoints/omni_base.py
@@ -17,7 +17,7 @@ from vllm.v1.engine.exceptions import EngineDeadError, EngineGenerateError
 from vllm_omni.engine.async_omni_engine import AsyncOmniEngine
 from vllm_omni.entrypoints.client_request_state import ClientRequestState
 from vllm_omni.entrypoints.pd_utils import PDDisaggregationMixin
-from vllm_omni.entrypoints.utils import coerce_param_message_types, detect_explicit_cli_keys, get_final_stage_id_for_e2e
+from vllm_omni.entrypoints.utils import coerce_param_message_types, get_final_stage_id_for_e2e
 from vllm_omni.metrics.stats import OrchestratorAggregator as OrchestratorMetrics
 from vllm_omni.model_executor.model_loader.weight_utils import download_weights_from_hf_specific
 from vllm_omni.outputs import OmniRequestOutput
