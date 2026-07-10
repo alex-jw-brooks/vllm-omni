1768034da867225b255ae1859760c68b27682294
Alex Brooks <albrooks@redhat.com>
2026-07-10 07:19:51 +0000

    add failing test for async chunk default
    Signed-off-by: Alex Brooks <albrooks@redhat.com>

--- tests/config/test_omni_config.py
+++ tests/config/test_omni_config.py
@@ -33,6 +33,7 @@ from vllm_omni.config.omni_config import (
     OmniStageParallelConfig,
     OmniStageRuntimeConfig,
     OmniStageSchedulerConfig,
+    StagePipelineConfig,
     VllmOmniARStageConfig,
     VllmOmniConfig,
     VllmOmniDiffusionStageConfig,
@@ -1705,3 +1706,31 @@ def test_compact_offload_config_is_validated_during_projection():
                 "components": ["dit"],
             }
         )
+
+
+def test_async_chunk_auto_disabled_without_processor():
+    """Ensure a multi-stage model that doesn't support async chunk turns it off by default."""
+    pipeline = PipelineConfig(
+        model_type="test_no_async",
+        model_arch="TestNoAsync",
+        stages=(
+            StagePipelineConfig(
+                stage_id=0,
+                model_stage="ar",
+                execution_type=StageExecutionType.LLM_AR,
+                final_output=True,
+            ),
+            StagePipelineConfig(
+                stage_id=1,
+                model_stage="generation",
+                execution_type=StageExecutionType.LLM_GENERATION,
+                input_sources=(0,),
+            ),
+        ),
+    )
+
+    deploy = DeployConfig()
+    # async chunk should not try to default to True in this case,
+    # since doing so will just raise a ValueError in validation.
+    merge_pipeline_deploy(pipeline, deploy)
+    assert not deploy.async_chunk
