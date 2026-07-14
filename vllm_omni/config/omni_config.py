dcfd4285107ce94b5e718900df758fa04192e591
Alex Brooks <albrooks@redhat.com>
2026-07-14 07:44:56 +0000

    make operation order consistent
    Signed-off-by: Alex Brooks <albrooks@redhat.com>

--- vllm_omni/config/omni_config.py
+++ vllm_omni/config/omni_config.py
@@ -1956,6 +1956,7 @@ class VllmOmniConfig:
 
         deploy = _apply_platform_overrides(deploy)
         deploy_by_id = {stage.stage_id: stage for stage in deploy.stages}
+        deploy.async_chunk = get_default_async_chunk_enabled(pipeline, deploy)
         model = cli_overrides.get("model")
 
         stage_configs = tuple(
