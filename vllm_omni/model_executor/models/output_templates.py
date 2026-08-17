aed4ff9244524cc39b4bf041bcc2458604bffd6d
Alex Brooks <albrooks@redhat.com>
2026-08-17 16:44:06 +0000

    make text hidden states type optional
    Signed-off-by: Alex Brooks <albrooks@redhat.com>

--- vllm_omni/model_executor/models/output_templates.py
+++ vllm_omni/model_executor/models/output_templates.py
@@ -9,7 +9,7 @@ from vllm_omni.data_entry_keys import OmniPayload
 class OmniOutput(NamedTuple):
     """Output from the merged Omni model containing both text and audio."""
 
-    text_hidden_states: torch.Tensor
+    text_hidden_states: torch.Tensor | None
     multimodal_outputs: OmniPayload | None = None
     intermediate_tensors: IntermediateTensors | None = None
     next_token_id: torch.Tensor | None = None
