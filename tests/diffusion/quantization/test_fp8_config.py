b971fab856c76bb82eb690a347ac6ed40cf32f6a
Alex Brooks <albrooks@redhat.com>
2026-08-27 04:53:43 +0000

    fix licenses
    Signed-off-by: Alex Brooks <albrooks@redhat.com>

--- tests/diffusion/quantization/test_fp8_config.py
+++ tests/diffusion/quantization/test_fp8_config.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
-# SPDX-FileCopyrightText: Copyright contributors to the vLLM project
+# SPDX-FileCopyrightText: Copyright contributors to the vLLM-Omni project
 """Tests for the unified quantization framework."""
 
 from types import SimpleNamespace
--- vllm_omni/quantization/mxfp4_config.py
+++ vllm_omni/quantization/mxfp4_config.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
-# SPDX-FileCopyrightText: Copyright contributors to the vLLM project
+# SPDX-FileCopyrightText: Copyright contributors to the vLLM-Omni project
 """W4A4 MXFP4 (Microscaling FP4) online/offline quantization for diffusion transformers.
 
 Architecture mirrors mxfp8_config.py:
